8de0c91bf287c93b24358e0832a71ef4f445a6f8
App Academy Student <appacademy@Apps-Mac-mini-192.local>
2016-09-15 11:56:44 -0400

    Completed enumerables and iterations

--- intro_js.js
+++ intro_js.js
@@ -1,7 +1,7 @@
 Array.prototype.uniq = function () {
   let arr = [];
   for (i = 0; i < this.length; i++) {
-    if (!arr.includes(this[i])) {
+    if (arr.indexOf(this[i]) === -1) {
       arr.push(this[i]);
     }
   }
@@ -42,3 +42,83 @@ Array.prototype.myTranspose = function () {
 
 let sqArr = [[-1, 1, 2], [3, 1, -1], [4, 8, 9]];
 console.log(sqArr.myTranspose());
+
+
+Array.prototype.myEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+};
+
+// sumArr.myEach( (el) => { console.log(2 * el); }
+  // );
+
+// sumArr.myEach( function(el) {
+//     console.log(el * 2);
+//   }
+// );
+
+function func(el) {
+  return el * 2;
+}
+
+sumArr.myEach(func);
+
+Array.prototype.myMap = function (callback) {
+  let arr = [];
+  this.myEach( (el) => {  arr.push(callback(el));  } );
+  return arr;
+};
+
+console.log(sumArr.myMap(func));
+
+Array.prototype.myInject = function (callback) {
+  let accum = this[0];
+
+  this.slice(1).myEach((el) => accum = callback(accum, el));
+
+  return accum;
+};
+
+function _add(acc, el) {
+  return acc + el;
+}
+
+console.log(sumArr.myInject(_add));
+
+
+Array.prototype.bubbleSort = function () {
+
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] > this[i + 1]) {
+        let temp = this[i+1];
+        this[i+1] = this[i];
+        this[i] = temp;
+        sorted = false;
+      }
+    }
+  }
+  console.log(this);
+
+};
+
+let unsorted = [5,8,2,6,8,45,2,3,6,-8,2];
+unsorted.bubbleSort();
+
+
+String.prototype.substrings = function() {
+
+  let substringsArr = [];
+  for (let startingIndex = 0; startingIndex < this.length; startingIndex++) {
+    for (let endingIndex = startingIndex; endingIndex < this.length; endingIndex++) {
+      substringsArr.push( this.slice(startingIndex, endingIndex + 1) );
+    }
+  }
+  return substringsArr.uniq();
+};
+
+
+console.log("catacat".substrings());
